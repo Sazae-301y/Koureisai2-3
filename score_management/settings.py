9feb7cdaa40d33bf93c9fc919e1554ac82b4d19b
Sazae-301y <topaz.yuttyadx@gmail.com>
2024-10-10 23:30:06 +0900

    a

--- score_management/settings.py
+++ score_management/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
