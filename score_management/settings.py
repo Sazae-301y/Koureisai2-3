55b1db916b4596643527a396e34b38b5ea3560be
Sazae-301y <topaz.yuttyadx@gmail.com>
2024-10-10 23:16:36 +0900

    cache

--- score_management/settings.py
+++ score_management/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
